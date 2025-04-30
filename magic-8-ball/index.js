a47a5f9184b15743be69ef9e8e1436bdfc7c9770
sarah_s <sarahlib90@gmail.com>
2025-04-29 21:22:55 -0400

    Initial commit

---
+++ magic-8-ball/index.js
@@ -0,0 +1,32 @@
+const userName = '';
+
+// ternary expression that decides what to do if the user enters a name or not
+if (userName == true) {
+    console.log(`Hello, ${userName}!`)
+}
+else {console.log('Hello!')}
+
+const userQuestion = '';
+console.log(userQuestion);
+
+const randomNumber = Math.floor(Math.random() * 8);
+
+const eightBall = '';
+
+if (randomNumber == 0) {
+    console.log('It is certain.');
+} else if (randomNumber == 1) {
+    console.log('It is decidedly so.');
+} else if (randomNumber == 2) {
+    console.log('Reply hazy try again.');
+} else if (randomNumber == 3) {
+    console.log('Cannot predict now.');
+} else if (randomNumber == 4) {
+    console.log('Do not count on it.');
+} else if (randomNumber == 5) {
+    console.log('My sources say no.');
+} else if (randomNumber == 6) {
+    console.log('Outlook not so good.');
+} else if (randomNumber == 7) {
+    console.log('Signs point to yes.');
+}
