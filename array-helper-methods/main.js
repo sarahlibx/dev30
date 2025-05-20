caa8b2d596029e12e395f3c2a08f47360f9f449f
sarah_s <sarahlib90@gmail.com>
2025-05-20 11:16:47 -0400

    Completed find lesson

--- array-helper-methods/main.js
+++ array-helper-methods/main.js
@@ -93,6 +93,63 @@ var pluck = function (array, property) {
   });
 };
 
-console.log(pluck(users, "email")); 
+// console.log(pluck(users, "email")); 
 
 // returns ['bob@gmail.com', `cindy@gmail.com`, `susan@gmail.com`, `sarah@gmail.com`, `tim@gmail.com` ];
+
+// find user whose username is "JimBob59"
+var users = [
+  { id: 1, username: 'Susan19', admin: false },
+  { id: 2, username: 'Antonio10', admin: false },
+  { id: 3, username: 'Fred15', admin: true },
+  { id: 4, username: 'Tim43', admin: false },
+  { id: 5, username: 'JimBob59', admin: true },
+  { id: 6, username: 'Clark6', admin: false },
+  { id: 7, username: 'Jameson35', admin: true },
+  { id: 8, username: 'Jessica40', admin: false },
+  { id: 9, username: 'Jaleesa13', admin: false },
+  { id: 10, username: 'Yoni45', admin: false }
+]
+
+// Set that user object to a variable called selectedUser
+var selectedUser = users.find(function (user) {
+    return user.username === 'JimBob59';
+});
+
+// console.log(selectedUser);
+
+// return which item in the array has a price that's less than 100
+var shoes = [
+  { name: 'Nike', price: 200 },
+  { name: 'Red Wings', price: 250 },
+  { name: 'Vans', price: 150 },
+  { name: 'Converse', price: 160 },
+  { name: 'Reebok', price: 130 },
+  { name: 'New Balance', price: 175 },
+  { name: 'Adidas', price: 95 },
+  { name: 'Keds', price: 140 },
+  { name: 'Crocs', price: 135 }
+];
+
+// Set this value to a variable called lessThan100
+var lessThan100 = shoes.find(function (shoe) {
+    return shoe.price < 100; 
+});
+
+// console.log(lessThan100);
+
+// result { name: 'Adidas', price: 95 }
+
+var findWhere = function (array, objCriteria) {
+  var property = Object.keys(objCriteria)[0];
+
+  var result = array.find(function(element) {
+    return element[property] === objCriteria[property]; 
+  });
+
+  return result;
+}
+
+console.log(findWhere(shoes, {price: 95}));
+
+var myShoe = findWhere(shoes, {price: 95}); 
